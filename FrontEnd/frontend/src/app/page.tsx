3c2d47f448976122d68c4738fa83920fae4dd2c2
Gino <ginoalodar@gmail.com>
2024-12-05 20:05:09 -0500

    feat: maketeado principal para el frontend

--- FrontEnd/frontend/src/app/page.tsx
+++ FrontEnd/frontend/src/app/page.tsx
@@ -2,7 +2,8 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1> FRONT-END para prueba tecnica de Grow Analytics</h1>
+      <h1 className="text-5xl font-bold"> FRONT-END para prueba tecnica de Grow Analytics</h1>
+      <p className="mt-4">Por favor ingrese sus credenciales para accesar al sistema.</p>
     </div>
   );
 }
